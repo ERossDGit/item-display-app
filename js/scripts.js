fc39ead7dfac2b4936f276697d845cc8e7418cb1
ERossDGit <50912378+ERossDGit@users.noreply.github.com>
2019-06-01 22:32:40 -0400

    Add repository of three Pokemon characters and output their names and height to DOM

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,17 @@
-alert('Hello world');
+var repository = [
+  {name: 'Bulbasaur', height: 0.7, types: ['grass', 'poison']},
+  {name: 'Spearow', height: 0.3, types: ['flying', 'normal']},
+  {name: 'Zubat', height: 0.8, types: ['poison', 'flying']}
+];
+
+var tempName
+
+for (var i = 0; i < 3; i++) {
+  tempName = repository[i].name + " (height: " + repository[i].height + ")";
+  if (repository[i].height > .5) {
+    tempName = tempName + " - Wow, that's big! <br>";
+  } else {
+    tempName = tempName + " <br>"
+  }
+  document.write(tempName);
+}
