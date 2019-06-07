c03a91c28b2040da23abf228181d4e8322eb3630
ERossDGit <50912378+ERossDGit@users.noreply.github.com>
2019-06-07 09:11:37 -0400

    wrap repository in an IIFE and add test for adding an object to the repository

--- js/scripts.js
+++ js/scripts.js
@@ -1,27 +1,43 @@
-var repository = [
-  {name: 'Bulbasaur', height: 0.7, types: ['grass', 'poison']},
-  {name: 'Spearow', height: 0.3, types: ['flying', 'normal']},
-  {name: 'Zubat', height: 0.8, types: ['poison', 'flying']}
-];
+// set up the pokemon registry by wrapping in an IIFE
+var pokemonRepository = (function () {
+  var repository = [
+    {name: 'Bulbasaur', height: 0.7, types: ['grass', 'poison']},
+    {name: 'Spearow', height: 0.3, types: ['flying', 'normal']},
+    {name: 'Zubat', height: 0.8, types: ['poison', 'flying']}
+  ];
 
+  function add(pokemon) {
+    if (typeof pokemon === 'object') {
+      repository.push(pokemon);
+    }
+  }
+
+  function getAll() {
+    return repository;
+  }
+
+  return {
+    add: add,
+    getAll: getAll
+  };
+})();
+
+// add a pokemon character to the registry
+pokemonRepository.add({name: 'Bugs Bunny', height: 10.5, types: ['rascally', 'rabbit']});
+// pokemonRepository.add('Bugs Bunny');  // this is a test case for trying to enter a non-object
+
+// temp variable to hold information to written to page
 var tempName
 
-// for (var i = 0; i < repository.length; i++) {
-//   tempName = repository[i].name + " (height: " + repository[i].height + ")";
-//   if (repository[i].height > .5) {
-//     tempName = tempName + " - Wow, that's big! <br>";
-//   } else {
-//     tempName = tempName + " <br>"
-//   }
-//   document.write(tempName);
-// }
-
-repository.forEach(function(currentItem) {
-  tempName = currentItem.name + " (height: " + currentItem.height + ")";
-  if (currentItem.height > .5) {
-    tempName = tempName + " - WOW, that's big! <br>";
+// loop through the repository and write each pokemon's
+// name and height
+pokemonRepository.getAll().forEach(function(currentItem) {
+  if (currentItem.height > 10) {
+    tempName = currentItem.name + " (height: " + currentItem.height + ") - WOW, that's humongous! Are you sure that's a pokemon character? <br>";
+  } else if (currentItem.height > .7) {
+    tempName = currentItem.name + " (height: " + currentItem.height + ") - WOW, that's big! <br>";
   } else {
-    tempName = tempName + " <br>"
-  }
+    tempName = currentItem.name + " (height: " + currentItem.height + ") <br>";
+    }
   document.write(tempName);
 });
